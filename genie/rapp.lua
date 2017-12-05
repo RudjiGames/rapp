a8afc3cb74115076f580743e39fb1752eea35f9d
Milos Tosic <milostosic77@gmail.com>
2017-12-05 13:49:49 +0800

    Add GENie configurations to support job schedulers

--- genie/rapp.lua
+++ genie/rapp.lua
@@ -3,21 +3,50 @@
 -- License: http://www.opensource.org/licenses/BSD-2-Clause
 --
 
+newoption {
+	trigger	= "scheduler",
+	value	= "Job scheduler type",
+	description = "Choose type of job scheduler",
+	allowed = {
+		{ "none",  "Serial execution"  },
+		{ "tbb",  "Intel Thread Building Blocks"  }
+	}
+}
+
+if  _OPTIONS["scheduler"] == nil then
+	_OPTIONS["scheduler"] = "tbb"
+end
+
+
+function add_scheduler_defines(definesToAppend)
+	if  _OPTIONS["scheduler"] == "tbb" then
+		return mergeTables(definesToAppend, {"RAPP_WITH_TBB=1"})
+	end
+	return definesToAppend
+end
+
 function projectDependencies_rapp()
-	return { "rbase" }
+	if _OPTIONS["scheduler"] ~= "tbb" then
+		return { "rbase" }
+	else
+		return { "rbase", "tbb" }
+	end
 end
 
 function projectAdd_rapp() 
-	addProject_lib("rapp")
+	addProject_lib("rapp", Lib.Runtime, false, nil, nil, nil, nil, add_scheduler_defines())
 end
 
+
+
 function projectDependencies_rapp_bgfx()
-	return { "rbase", "bx", "bimg", "bgfx" }
+	return mergeTables( projectDependencies_rapp(), { "bx", "bimg", "bgfx" } )
 end
 
 function projectAdd_rapp_bgfx()
-	addProject_lib("rapp", Lib.Runtime, false, nil, nil, nil, nil, "RAPP_WITH_BGFX=1", "_bgfx")
+	addProject_lib("rapp", Lib.Runtime, false, nil, nil, nil, nil, add_scheduler_defines({ "RAPP_WITH_BGFX=1" }), "_bgfx")
 end
+
 
 function projectDependencyConfig_02_bgfx(_dependency)
 	if _dependency == "rapp" then return {"rapp", "bgfx"} end
