648143c60ee8ac64814f5c69a6de5de1185bc6fb
Milos Tosic <milostosic77@gmail.com>
2023-04-20 11:35:00 +0200

    cleanup kbd code and use default colors for console

--- inc/widgets/keyboard.h
+++ inc/widgets/keyboard.h
@@ -15,39 +15,43 @@
 
 namespace rapp {
 
-	constexpr float BUTTON_SIZE_X	= 39.0f;
-	constexpr float BUTTON_SIZE_Y	= 30.0f;
-	constexpr float BUTTON_SPACING	= 3.0f;
+	constexpr float RAPP_VK_BUTTON_SIZE_X	= 39.0f;
+	constexpr float RAPP_VK_BUTTON_SIZE_Y	= 30.0f;
+	constexpr float RAPP_VK_BUTTON_SPACING	= 3.0f;
 
-	static void rappKeyboardLine(const char* key, KeyboardState::Key* _keyMap, float _scale)
+	static void rappVirtualKeyboardClick(KeyboardState::Key _key)
+	{
+		inputEmitKeyPress(_key);
+		ImGui::SetKeyboardFocusHere(-1); // TODO: kbd should have no focus
+	}
+
+	static void rappVirtualKeyboardLine(const char* key, KeyboardState::Key* _keyMap, float _scale)
     {
         size_t num = rtm::strLen(key);
         for (size_t i=0; i<num; i++)
         {
 			char key_label[] = "X";
 			key_label[0] = *key;
-			if (ImGui::Button(key_label, ImVec2(BUTTON_SIZE_X * _scale, BUTTON_SIZE_Y * _scale)))
-				inputEmitKeyPress(_keyMap[i]);
+			if (ImGui::Button(key_label, ImVec2(RAPP_VK_BUTTON_SIZE_X * _scale, RAPP_VK_BUTTON_SIZE_Y * _scale)))
+				rappVirtualKeyboardClick(_keyMap[i]);
 			key++;
 			if (i<num-1)
-				ImGui::SameLine(0.0f, BUTTON_SPACING * _scale);
+				ImGui::SameLine(0.0f, RAPP_VK_BUTTON_SPACING * _scale);
         }
     };
 
-	static int rappVirtualKeyboard()
+	static void rappVirtualKeyboard(float _scale = 1.0f)
 	{
-		ImGui::Begin("Virtual Keyboard", 0, ImGuiWindowFlags_NoResize);
-
-		float scale = 1.0f;
+		ImGui::Begin("Virtual Keyboard", 0, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoTitleBar);
 
 		KeyboardState::Key keys1[] = {
 			KeyboardState::Tilde, KeyboardState::Key1, KeyboardState::Key2, KeyboardState::Key3, KeyboardState::Key4, KeyboardState::Key5, KeyboardState::Key6,
 			KeyboardState::Key7, KeyboardState::Key8, KeyboardState::Key9, KeyboardState::Key0, KeyboardState::Minus, KeyboardState::Plus };
 
-		rappKeyboardLine("~1234567890-+", keys1, scale);
-		ImGui::SameLine(0.0f, BUTTON_SPACING * scale);
+		rappVirtualKeyboardLine("~1234567890-+", keys1, _scale);
+		ImGui::SameLine(0.0f, RAPP_VK_BUTTON_SPACING * _scale);
 		if (ImGui::ArrowButton("<-", ImGuiDir_Left))
-			inputEmitKeyPress(KeyboardState::Backspace);
+			rappVirtualKeyboardClick(KeyboardState::Backspace);
 
 		KeyboardState::Key keys2[] = {
 			KeyboardState::KeyQ, KeyboardState::KeyW, KeyboardState::KeyE, KeyboardState::KeyR, KeyboardState::KeyT, KeyboardState::KeyY, KeyboardState::KeyU,
@@ -55,7 +59,7 @@ namespace rapp {
 
 		ImGui::Text("   ");
 		ImGui::SameLine();
-		rappKeyboardLine("qwertyuiop[]\\", keys2, scale);
+		rappVirtualKeyboardLine("qwertyuiop[]\\", keys2, _scale);
 
 		KeyboardState::Key keys3[] = {
 			KeyboardState::KeyA, KeyboardState::KeyS, KeyboardState::KeyD, KeyboardState::KeyF, KeyboardState::KeyG, KeyboardState::KeyH,
@@ -63,10 +67,10 @@ namespace rapp {
 
 		ImGui::Text("      ");
 		ImGui::SameLine();
-		rappKeyboardLine("asdfghjkl;'", keys3, scale);
-		ImGui::SameLine(0.0f, BUTTON_SPACING * scale);
-		if (ImGui::Button("Return", ImVec2(BUTTON_SIZE_X * 1.85f * scale, BUTTON_SIZE_Y * scale)))
-			inputEmitKeyPress(KeyboardState::Return);
+		rappVirtualKeyboardLine("asdfghjkl;'", keys3, _scale);
+		ImGui::SameLine(0.0f, RAPP_VK_BUTTON_SPACING * _scale);
+		if (ImGui::Button("Return", ImVec2(RAPP_VK_BUTTON_SIZE_X * 1.85f * _scale, RAPP_VK_BUTTON_SIZE_Y * _scale)))
+			rappVirtualKeyboardClick(KeyboardState::Return);
 
 		KeyboardState::Key keys4[] = {
 			KeyboardState::KeyZ, KeyboardState::KeyX, KeyboardState::KeyC, KeyboardState::KeyV, KeyboardState::KeyB, KeyboardState::KeyN, KeyboardState::KeyM,
@@ -74,14 +78,13 @@ namespace rapp {
 
 		ImGui::Text("         ");
 		ImGui::SameLine();
-		rappKeyboardLine("zxcvbnm,.?", keys4, scale);
+		rappVirtualKeyboardLine("zxcvbnm,.?", keys4, _scale);
 		ImGui::Text("                                          ");
-		ImGui::SameLine(0.0f, BUTTON_SPACING * scale);
-		if (ImGui::Button("---------", ImVec2(BUTTON_SIZE_X * 6.0f * scale, BUTTON_SIZE_Y * scale)))
-			inputEmitKeyPress(KeyboardState::Space);
+		ImGui::SameLine(0.0f, RAPP_VK_BUTTON_SPACING * _scale);
+		if (ImGui::Button("---------", ImVec2(RAPP_VK_BUTTON_SIZE_X * 6.0f * _scale, RAPP_VK_BUTTON_SIZE_Y * _scale)))
+			rappVirtualKeyboardClick(KeyboardState::Space);
 
 		ImGui::End();
-		return 0;
 	}
 
 } // namespace rapp
