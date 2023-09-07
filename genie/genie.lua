c708554145a3b3cd00c9347072720ecb03c6902f
Milos Tosic <milostosic77@gmail.com>
2023-09-07 10:09:31 +0200

    Fix genie scripts

--- genie/genie.lua
+++ genie/genie.lua
@@ -9,8 +9,8 @@ while string.len(currPath) > 2 do
 	if os.isfile(currPath .. "/build/build.lua") then dofile (currPath .. "/build/build.lua") break end
 end
 
-solution "rbase"
+solution "rapp"
 	configurations { "debug", "release", "retail" }
 	setPlatforms()
 
-	addLibProjects("rbase")
+	addLibProjects("rapp")
--- genie/rbase.lua
+++
@@ -1,8 +0,0 @@
---
--- Copyright 2023 Milos Tosic. All rights reserved.
--- License: http://www.opensource.org/licenses/BSD-2-Clause
---
-
-function projectAdd_rbase() 
-	addProject_lib("rbase")
-end
