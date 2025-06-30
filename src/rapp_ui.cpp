151b0f0640e20c7b05ecc9d9ae99045a132cdedf
milos.tosic <milos@rudji.com>
2025-06-30 06:46:54 +0200

    Style wip

--- src/rapp_ui.cpp
+++ src/rapp_ui.cpp
@@ -57,23 +57,25 @@ namespace rapp {
 				#define RUDJI_COL_PURPLE			ImVec4(0.082f, 0.082f, 0.133f, 1.00f)
 				#define RUDJI_COL_PURPLE_DARK		ImVec4(0.050f, 0.050f, 0.082f, 1.00f)
 				#define RUDJI_COL_PURPLE_LIGHT		ImVec4(0.349f, 0.349f, 0.564f, 1.00f)
+				#define RUDJI_COL_BLUE_LIGHT		ImVec4(0.215f, 0.215f, 0.564f, 1.00f)
 				#define RUDJI_COL_GREEN				ImVec4(0.133f, 0.564f, 0.349f, 1.00f)
 				#define RUDJI_COL_GREEN_DARK		ImVec4(0.133f, 0.349f, 0.215f, 1.00f)
 				#define RUDJI_COL_RED				ImVec4(0.564f, 0.215f, 0.215f, 1.00f)
 				#define RUDJI_COL_RED_DARK			ImVec4(0.349f, 0.082f, 0.082f, 1.00f)
+				#define RUDJI_COL_CYAN_DARK			ImVec4(0.133f, 0.349f, 0.349f, .60f)
 				#define RUDJI_COL_CYAN				ImVec4(0.215f, 0.564f, 0.564f, 1.00f)
-				#define RUDJI_COL_CYAN_DARK			ImVec4(0.349f, 0.913f, 0.913f, 1.00f)
+				#define RUDJI_COL_CYAN_LIGHT		ImVec4(0.349f, 0.913f, 0.913f, 1.00f)
 				#define RUDJI_COL_PINK				ImVec4(0.564f, 0.349f, 0.564f, 1.00f)
 
 				ImGui::StyleColorsDark(&style);
 
 				style.Colors[ImGuiCol_Text]						= RUDJI_COL_WHITE;
 				style.Colors[ImGuiCol_TextDisabled]				= RUDJI_ALPHA(RUDJI_COL_WHITE,  0.6f);
-				style.Colors[ImGuiCol_WindowBg]					= RUDJI_ALPHA(RUDJI_COL_PURPLE, 0.9f);
+				style.Colors[ImGuiCol_WindowBg]					= RUDJI_ALPHA(RUDJI_COL_PURPLE, 0.93f);
 				style.Colors[ImGuiCol_ChildBg]					= RUDJI_COL_PURPLE_DARK;
-				style.Colors[ImGuiCol_PopupBg]					= RUDJI_COL_PURPLE_DARK;
-				style.Colors[ImGuiCol_Border]					= RUDJI_ALPHA(RUDJI_COL_PURPLE_LIGHT, 0.66f);
-				style.Colors[ImGuiCol_BorderShadow]				= RUDJI_COL_WHITE;
+				style.Colors[ImGuiCol_PopupBg]					= RUDJI_COL_PURPLE;
+				style.Colors[ImGuiCol_Border]					= RUDJI_ALPHA(RUDJI_COL_PURPLE_LIGHT, 0.36f);
+				style.Colors[ImGuiCol_BorderShadow]				= RUDJI_ALPHA(RUDJI_COL_PURPLE_LIGHT, 0.36f);
 				style.Colors[ImGuiCol_FrameBg]					= RUDJI_COL_BLACK;
 				style.Colors[ImGuiCol_FrameBgHovered]			= RUDJI_COL_PURPLE_DARK;
 				style.Colors[ImGuiCol_FrameBgActive]			= RUDJI_COL_PURPLE;
@@ -81,46 +83,46 @@ namespace rapp {
 				style.Colors[ImGuiCol_TitleBgActive]			= RUDJI_COL_BLACK;
 				style.Colors[ImGuiCol_TitleBgCollapsed]			= RUDJI_COL_PURPLE;
 				style.Colors[ImGuiCol_MenuBarBg]				= RUDJI_COL_PURPLE_DARK;
-				style.Colors[ImGuiCol_ScrollbarBg]				= RUDJI_COL_PURPLE_DARK;
-	//			style.Colors[ImGuiCol_ScrollbarGrab]			= RUDJI_COL_PURPLE_LIGHT;
-	//			style.Colors[ImGuiCol_ScrollbarGrabHovered]		= RUDJI_COL_GREEN_DARK;
-	//			style.Colors[ImGuiCol_ScrollbarGrabActive]		= RUDJI_COL_GREEN;
+				style.Colors[ImGuiCol_ScrollbarBg]				= RUDJI_COL_BLACK;
+				style.Colors[ImGuiCol_ScrollbarGrab]			= RUDJI_COL_PURPLE;
+				style.Colors[ImGuiCol_ScrollbarGrabHovered]		= RUDJI_COL_CYAN_DARK;
+				style.Colors[ImGuiCol_ScrollbarGrabActive]		= RUDJI_COL_CYAN;
 				style.Colors[ImGuiCol_CheckMark]				= RUDJI_COL_GREEN;
-	//			style.Colors[ImGuiCol_SliderGrab]				= RUDJI_COL_GREEN_DARK;
-	//			style.Colors[ImGuiCol_SliderGrabActive]			= RUDJI_COL_GREEN;
-	//			style.Colors[ImGuiCol_Button]					= ImVec4(0.26f, 0.59f, 0.98f, 0.40f);
-	//			style.Colors[ImGuiCol_ButtonHovered]			= ImVec4(0.26f, 0.59f, 0.98f, 1.00f);
-	//			style.Colors[ImGuiCol_ButtonActive]				= ImVec4(0.133f, 0.349f, 0.215f, 1.00f);
-	//			style.Colors[ImGuiCol_Header]					= ;
-	//			style.Colors[ImGuiCol_HeaderHovered]			= ;
-	//			style.Colors[ImGuiCol_HeaderActive]				= ;
-	//			style.Colors[ImGuiCol_Separator]				= ;
-	//			style.Colors[ImGuiCol_SeparatorHovered]			= ;
-	//			style.Colors[ImGuiCol_SeparatorActive]			= ;
-	//			style.Colors[ImGuiCol_ResizeGrip]				= ;
-	//			style.Colors[ImGuiCol_ResizeGripHovered]		= ;
-	//			style.Colors[ImGuiCol_ResizeGripActive]			= ;
-	//			style.Colors[ImGuiCol_InputTextCursor]			= ;
-	//			style.Colors[ImGuiCol_TabHovered]				= ;
-	//			style.Colors[ImGuiCol_Tab]						= ;
-	//			style.Colors[ImGuiCol_TabSelected]				= ;
+				style.Colors[ImGuiCol_SliderGrab]				= RUDJI_COL_CYAN_DARK;
+				style.Colors[ImGuiCol_SliderGrabActive]			= RUDJI_COL_CYAN;
+				style.Colors[ImGuiCol_Button]					= RUDJI_COL_CYAN_DARK;
+				style.Colors[ImGuiCol_ButtonHovered]			= RUDJI_COL_CYAN;
+				style.Colors[ImGuiCol_ButtonActive]				= RUDJI_COL_CYAN_LIGHT;
+				style.Colors[ImGuiCol_Header]					= RUDJI_ALPHA(RUDJI_COL_CYAN_DARK, 0.5f);
+				style.Colors[ImGuiCol_HeaderHovered]			= RUDJI_COL_CYAN;
+				style.Colors[ImGuiCol_HeaderActive]				= RUDJI_COL_CYAN_LIGHT;
+				style.Colors[ImGuiCol_Separator]				= RUDJI_COL_CYAN_DARK;
+				style.Colors[ImGuiCol_SeparatorHovered]			= RUDJI_COL_CYAN;
+				style.Colors[ImGuiCol_SeparatorActive]			= RUDJI_COL_CYAN_LIGHT;
+				style.Colors[ImGuiCol_ResizeGrip]				= RUDJI_COL_CYAN;
+				style.Colors[ImGuiCol_ResizeGripHovered]		= RUDJI_COL_GREEN_DARK;
+				style.Colors[ImGuiCol_ResizeGripActive]			= RUDJI_COL_GREEN;
+				style.Colors[ImGuiCol_InputTextCursor]			= RUDJI_COL_CYAN;
+				style.Colors[ImGuiCol_TabHovered]				= RUDJI_COL_CYAN;
+				style.Colors[ImGuiCol_Tab]						= RUDJI_COL_BLACK;
+				style.Colors[ImGuiCol_TabSelected]				= RUDJI_COL_CYAN_DARK;
 	//			style.Colors[ImGuiCol_TabSelectedOverline]		= ;
 	//			style.Colors[ImGuiCol_TabDimmed]				= ;
 	//			style.Colors[ImGuiCol_TabDimmedSelected]		= ;
 	//			style.Colors[ImGuiCol_TabDimmedSelectedOverline]= ;
 	//			style.Colors[ImGuiCol_DockingPreview]			= ;
 	//			style.Colors[ImGuiCol_DockingEmptyBg]			= ;
-	//			style.Colors[ImGuiCol_PlotLines]				= ;
-	//			style.Colors[ImGuiCol_PlotLinesHovered]			= ;
-	//			style.Colors[ImGuiCol_PlotHistogram]			= ;
-	//			style.Colors[ImGuiCol_PlotHistogramHovered]		= ;
+				style.Colors[ImGuiCol_PlotLines]				= RUDJI_COL_CYAN;
+				style.Colors[ImGuiCol_PlotLinesHovered]			= RUDJI_COL_CYAN_LIGHT;
+				style.Colors[ImGuiCol_PlotHistogram]			= RUDJI_COL_CYAN;
+				style.Colors[ImGuiCol_PlotHistogramHovered]		= RUDJI_COL_CYAN_LIGHT;
 	//			style.Colors[ImGuiCol_TableHeaderBg]			= ;
 	//			style.Colors[ImGuiCol_TableBorderStrong]		= ;
 	//			style.Colors[ImGuiCol_TableBorderLight]			= ;
 	//			style.Colors[ImGuiCol_TableRowBg]				= ;
 	//			style.Colors[ImGuiCol_TableRowBgAlt]			= ;
-	//			style.Colors[ImGuiCol_TextLink]					= ;
-	//			style.Colors[ImGuiCol_TextSelectedBg]			= ;
+				style.Colors[ImGuiCol_TextLink]					= RUDJI_COL_CYAN_LIGHT;
+				style.Colors[ImGuiCol_TextSelectedBg]			= RUDJI_COL_CYAN_DARK;
 	//			style.Colors[ImGuiCol_TreeLines]				= ;
 	//			style.Colors[ImGuiCol_DragDropTarget]			= ;
 	//			style.Colors[ImGuiCol_NavCursor]				= ;
