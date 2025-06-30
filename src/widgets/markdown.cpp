2500a5c3937e5dcbf7af4454542f5f5ca1e2a5b6
Milos Tosic <milostosic77@gmail.com>
2025-06-30 14:56:47 +0200

    Fix osx compile

--- src/widgets/markdown.cpp
+++ src/widgets/markdown.cpp
@@ -6,7 +6,7 @@
 #include <rapp_pch.h>
 #include <rapp/inc/rapp.h>
 #include <rapp/inc/widgets/markdown.h>
-#include <rapp/3rd/imgui_markdown/imgui_markdown.h>
+#include "../../inc/widgets/markdown.h"
 
 #ifdef RAPP_WITH_BGFX
 
